fa089f87706313a17cc593d3c7bb6aafd422823c
gpoisoned <gpoisoned@gmail.com>
2015-04-19 17:05:16 -0600

    Remove puts statements

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,6 @@ class MainController < ApplicationController
   end
 
   def search
-    puts params.inspect
     @search_results = PgSearch.multisearch(params[:query]).paginate(:page => params[:page], :per_page => 15)
-    puts @search_results.inspect
   end
 end
